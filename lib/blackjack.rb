f41e966c4c91bd0a55fd3832eea45a04206ffd8d
Preston Elliott <preston.elliott24@gmail.com>
2019-03-07 21:09:29 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -19,11 +19,13 @@ def get_user_input
 end
 
 def end_game
-  # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card_total = deal_card + deal_card
+  display_card_total(card_total)
+  card_total
 end
 
 def hit?
